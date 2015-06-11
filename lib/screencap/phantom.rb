a84fd486d7f7edbab2c026ff8d4145565da4e155
John Brosnan <jbrosnan@constantcontact.com>
2015-06-11 14:52:23 -0400

    Allow for phantomjs params

--- lib/screencap/phantom.rb
+++ lib/screencap/phantom.rb
@@ -5,14 +5,23 @@ module Screencap
     RASTERIZE = SCREENCAP_ROOT.join('screencap', 'raster.js')
 
     def self.rasterize(url, path, args = {})
+      cloned_args = args.clone
+      phantom_params = get_phantom_params args.clone
       params = {
         url: CGI::escape(url),
         output: path
-      }.merge(args).collect {|k,v| "#{k}=#{v}"}
+      }.merge(cloned_args).collect {|k,v| "#{k}=#{v}"}
       puts RASTERIZE.to_s, params if(args[:debug])
-      result = Phantomjs.run(RASTERIZE.to_s, *params)
+      result = Phantomjs.run(phantom_params, RASTERIZE.to_s, *params)
       puts result if(args[:debug])
       raise Screencap::Error, "Could not load URL #{url}" if result.match /Unable to load/
+
+    end
+
+    def self.get_phantom_params(args)
+      opts = args[:phantomjs] || {}
+      args.delete(:phantomjs)
+      opts.collect {|k,v| "#{k}=#{v}"}.join(' ')
     end
 
     def quoted_args(args)
